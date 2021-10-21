712d552aa29371f1c0c9069ccfcb032e79ed1539
Edward Halferty <edhalferty@gmail.com>
2021-10-20 21:59:35 -0700

    stuff

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < GZ_FILE_SIZE; i++) {
         fputc(garchinstall[i], gzFile);
     }
+    system("tar -xzf garchinstall.tar.gz");
     return 0;
 }
