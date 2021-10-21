54e92d525f619892f6d24b3532189dff5cd17a65
Edward Halferty <edhalferty@gmail.com>
2021-10-21 03:45:12 -0700

    checkin

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -1,10 +1,11 @@
 print("Hello, world!");
-system("./installstuff.sh");
-# system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
-# system("pacstrap /mnt/installer-root base linux");
-# system("arch-chroot /mnt/installer-root");
-# system("pacman -Syu");
-# system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev --noconfirm");
+# system("./installstuff.sh");
+system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
+system("pacstrap /mnt/installer-root base linux");
+system("arch-chroot /mnt/installer-root");
+system("pacman -Syu");
+system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware xf86-input-evdev xf86-input-synaptics --noconfirm");
+system("bash")
 
 # mount -t tmpfs -o size=512m tmpfs /mnt/root1
 # pacstrap /mnt/root1 base linux
--- perl/installstuff.sh
+++ perl/installstuff.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/bash
 
-mkdir /mnt/installer-root
-mount -t tmpfs -o size=4g tmpfs /mnt/installer-root
-cp -r /bin /mnt/installer-root
-cp -r /boot /mnt/installer-root
-mkdir /mnt/installer-root/dev
-cp -r /etc /mnt/installer-root
-cp -r /home /mnt/installer-root
-cp -r /lib /mnt/installer-root
-cp -r /lib64 /mnt/installer-root
-mkdir /mnt/installer-root/mnt
-cp -r /opt /mnt/installer-root
-mkdir /mnt/installer-root/proc
-cp -r /root /mnt/installer-root
-cp -r /run /mnt/installer-root
-cp -r /sbin /mnt/installer-root
-cp -r /srv /mnt/installer-root
-mkdir /mnt/installer-root/sys
-cp -r /tmp /mnt/installer-root
-cp -r /usr /mnt/installer-root
-cp -r /var /mnt/installer-root
-cp -r /version /mnt/installer-root
-arch-chroot /mnt/installer-root
-pacman -Syu
-pacman -Sy which xorg xorg-xinit xorg-xeyes xorg-drivres --noconfirm
+# mkdir /mnt/installer-root
+# mount -t tmpfs -o size=4g tmpfs /mnt/installer-root
+# cp -r /bin /mnt/installer-root
+# cp -r /boot /mnt/installer-root
+# mkdir /mnt/installer-root/dev
+# cp -r /etc /mnt/installer-root
+# cp -r /home /mnt/installer-root
+# cp -r /lib /mnt/installer-root
+# cp -r /lib64 /mnt/installer-root
+# mkdir /mnt/installer-root/mnt
+# cp -r /opt /mnt/installer-root
+# mkdir /mnt/installer-root/proc
+# cp -r /root /mnt/installer-root
+# cp -r /run /mnt/installer-root
+# cp -r /sbin /mnt/installer-root
+# cp -r /srv /mnt/installer-root
+# mkdir /mnt/installer-root/sys
+# cp -r /tmp /mnt/installer-root
+# cp -r /usr /mnt/installer-root
+# cp -r /var /mnt/installer-root
+# cp -r /version /mnt/installer-root
+# arch-chroot /mnt/installer-root
+# pacman -Syu
+# pacman -Sy which xorg xorg-xinit xorg-xeyes xorg-drivres --noconfirm
 
 # pacstrap /mnt/installer-root base linux
 # pacman -Syu
