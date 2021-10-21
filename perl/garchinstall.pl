a51a3c07ab009e5c5aa0032281e871dc0e2dfd06
Edward Halferty <edhalferty@gmail.com>
2021-10-21 05:18:16 -0700

    checkin

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -1,31 +1,31 @@
 print("Hello, world!");
 system("mkdir /mnt/installer-root");
-print("1")
+print("1");
 <STDIN>;
 system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
-print("2")
+print("2");
 <STDIN>;
 system("pacstrap /mnt/installer-root base linux");
-print("3")
+print("3");
 <STDIN>;
 system("arch-chroot /mnt/installer-root");
-print("4")
+print("4");
 <STDIN>;
 system("pacman -Syu");
-print("5")
+print("5");
 <STDIN>;
 system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware --noconfirm");
-print("6")
+print("6");
 <STDIN>;
 system("pacman -Sy xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm --noconfirm");
-print("7")
+print("7");
 <STDIN>;
 system("unset DBUS_SESSION_BUS_ADDRESS");
-print("8")
+print("8");
 <STDIN>;
 system("systemctl enable lightdm");
 # system("startxfce4")
-print("9")
+print("9");
 <STDIN>;
 system("startxfce4");
 
