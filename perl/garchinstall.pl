fe185a2aa06bc0eed2e6313989ba5132720e226c
Edward Halferty <edhalferty@gmail.com>
2021-10-20 23:59:30 -0700

    stuff

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -4,7 +4,7 @@ system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
 system("pacstrap /mnt/installer-root base linux");
 system("arch-chroot /mnt/installer-root");
 system("pacman -Syu");
-system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev --noconfirm");
+# system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev --noconfirm");
 
 # mount -t tmpfs -o size=512m tmpfs /mnt/root1
 # pacstrap /mnt/root1 base linux
