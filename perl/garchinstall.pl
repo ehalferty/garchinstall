7fd9c6212e0a0e4af170d9c0c5809f811e4a7d36
Edward Halferty <edhalferty@gmail.com>
2021-10-20 23:01:43 -0700

    stuff

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -1,10 +1,11 @@
 print("Hello, world!")
-system("mkdir /mnt/installer-root")
-system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root")
-system("pacstrap /mnt/installer-root base linux")
-system("arch-chroot /mnt/installer-root")
-system("pacman -Syu")
-system("pacman -Sy xfce4")
+system("ls")
+# system("mkdir /mnt/installer-root")
+# system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root")
+# system("pacstrap /mnt/installer-root base linux")
+# system("arch-chroot /mnt/installer-root")
+# system("pacman -Syu")
+# system("pacman -Sy xfce4")
 
 # mount -t tmpfs -o size=512m tmpfs /mnt/root1
 # pacstrap /mnt/root1 base linux
