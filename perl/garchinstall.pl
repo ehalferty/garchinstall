5727549bec8db289dc9f58d1490f31dfc9789df9
Edward Halferty <edhalferty@gmail.com>
2021-10-21 05:04:13 -0700

    checkin

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -1,11 +1,15 @@
 print("Hello, world!");
-# system("./installstuff.sh");
+system("mkdir /mnt/installer-root");
 system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
 system("pacstrap /mnt/installer-root base linux");
 system("arch-chroot /mnt/installer-root");
 system("pacman -Syu");
-system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware xf86-input-evdev xf86-input-synaptics --noconfirm");
-system("bash")
+system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware --noconfirm");
+system("pacman -Sy xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm --noconfirm");
+system("unset DBUS_SESSION_BUS_ADDRESS")
+system("systemctl enable lightdm")
+# system("startxfce4")
+system("startxfce4")
 
 # mount -t tmpfs -o size=512m tmpfs /mnt/root1
 # pacstrap /mnt/root1 base linux
