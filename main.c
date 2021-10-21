d4040f3071fae37dc29b0933fcd745b05647c7ce
Edward Halferty <edhalferty@gmail.com>
2021-10-21 00:47:20 -0700

    stuff

--- main.c
+++ main.c
@@ -6,10 +6,12 @@ int main(int argc, char *argv[]) {
     }
     fclose(gzFile);
     system("tar -xzf garchinstall.tar.gz");
-    char *eargs[2];
+    char *eargs[3];
     eargs[0] = malloc(128);
-    eargs[1] = 0;
-    sprintf(eargs[0], "perl/garchinstall.pl");
+    eargs[1] = malloc(128);
+    eargs[2] = 0;
+    sprintf(eargs[0], "/usr/bin/perl");
+    sprintf(eargs[1], "perl/garchinstall.pl");
     execve("/usr/bin/perl", eargs, NULL);
     // system("perl perl/garchinstall.pl");
     return 0;
