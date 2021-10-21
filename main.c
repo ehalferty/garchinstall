4563afaa4290e8892965825e5bc8ae5a85bd49fe
Edward Halferty <edhalferty@gmail.com>
2021-10-20 22:58:52 -0700

    stuff

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < GZ_FILE_SIZE; i++) {
         fputc(garchinstall[i], gzFile);
     }
+    fclose(gzFile);
     system("tar -xzf garchinstall.tar.gz");
     system("perl garchinstall/garchinstall.pl");
     return 0;
