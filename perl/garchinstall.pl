010bda725f7e90f23691425b1ad6b59e2b8c3038
Edward Halferty <edhalferty@gmail.com>
2021-10-21 05:22:01 -0700

    checkin

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -8,26 +8,26 @@ print("2");
 system("pacstrap /mnt/installer-root base linux");
 print("3");
 <STDIN>;
-system("arch-chroot /mnt/installer-root");
+# system("arch-chroot /mnt/installer-root");
 print("4");
 <STDIN>;
-system("pacman -Syu");
+system("arch-chroot /mnt/installer-root pacman -Syu");
 print("5");
 <STDIN>;
-system("pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware --noconfirm");
+system("arch-chroot /mnt/installer-root pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware --noconfirm");
 print("6");
 <STDIN>;
-system("pacman -Sy xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm --noconfirm");
+system("arch-chroot /mnt/installer-root pacman -Sy xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm --noconfirm");
 print("7");
 <STDIN>;
-system("unset DBUS_SESSION_BUS_ADDRESS");
+system("arch-chroot /mnt/installer-root unset DBUS_SESSION_BUS_ADDRESS");
 print("8");
 <STDIN>;
-system("systemctl enable lightdm");
+system("arch-chroot /mnt/installer-root systemctl enable lightdm");
 # system("startxfce4")
 print("9");
 <STDIN>;
-system("startxfce4");
+system("arch-chroot /mnt/installer-root startxfce4");
 
 # mount -t tmpfs -o size=512m tmpfs /mnt/root1
 # pacstrap /mnt/root1 base linux
