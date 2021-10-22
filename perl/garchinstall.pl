672042f5a57097a6e80e2ac6a48fd6e87435d654
Edward Halferty <edhalferty@gmail.com>
2021-10-21 23:25:51 -0700

    checkin

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -1,14 +1,20 @@
 print("Hello, world!");
-# TODO: Check if we have network connectivity. If not, run the framebuffer-based network manager
-# TODO: Throw up a splash screen to tell the user that we're downloading dependencies...
 system("mkdir /mnt/installer-root");
 system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
 system("pacstrap /mnt/installer-root base linux");
 system("arch-chroot /mnt/installer-root pacman -Syu");
-system("arch-chroot /mnt/installer-root pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware --noconfirm");
-system("arch-chroot /mnt/installer-root pacman -Sy xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm --noconfirm");
-system("unset DBUS_SESSION_BUS_ADDRESS");
-system("arch-chroot /mnt/installer-root systemctl enable lightdm");
+system("arch-chroot /mnt/installer-root pacman -Sy glib2 pango --noconfirm");
+
+# TODO: Check if we have network connectivity. If not, run the framebuffer-based network manager
+# TODO: Throw up a splash screen to tell the user that we're downloading dependencies...
+# system("mkdir /mnt/installer-root");
+# system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
+# system("pacstrap /mnt/installer-root base linux");
+# system("arch-chroot /mnt/installer-root pacman -Syu");
+# system("arch-chroot /mnt/installer-root pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware --noconfirm");
+# system("arch-chroot /mnt/installer-root pacman -Sy xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm --noconfirm");
+# system("unset DBUS_SESSION_BUS_ADDRESS");
+# system("arch-chroot /mnt/installer-root systemctl enable lightdm");
 # system("arch-chroot /mnt/installer-root startxfce4");
 # system("/usr/bin/perl", "perl/garchinstall2.pl");
 
