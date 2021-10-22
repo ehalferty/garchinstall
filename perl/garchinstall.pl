4f21766f07ece8a7ba9798fbe984fa401515d446
Edward Halferty <edhalferty@gmail.com>
2021-10-22 01:51:27 -0700

    checkin

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -3,6 +3,7 @@ system("mkdir /mnt/installer-root");
 system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
 system("pacstrap /mnt/installer-root base linux");
 system("cp ./perl/garchinstall2.pl /mnt/installer-root/root/");
+system("cp ./perl/garchinstall3.pl /mnt/installer-root/root/");
 system("arch-chroot /mnt/installer-root pacman -Syu");
 system("arch-chroot /mnt/installer-root pacman -Sy glib2 pango gdk-pixbuf2 gtk2 gtk3 perl-gtk2 gtk3-perl weston --noconfirm");
 system("arch-chroot /mnt/installer-root perl /root/garchinstall2.pl");
