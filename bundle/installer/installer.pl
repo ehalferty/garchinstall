a584c9568e640d302d3d53c5076f9f38928a6f8a
Edward Halferty <edhalferty@gmail.com>
2021-10-22 03:47:51 -0700

    checkin

--- bundle/installer/installer.pl
+++ bundle/installer/installer.pl
@@ -1,1 +1,13 @@
-print("Hello, world!");
+print("Welcome to garchinstaller!");
+# system("mkdir /mnt/installer-root");
+# system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
+# system("pacstrap /mnt/installer-root base linux");
+# system("cp ./perl/garchinstall2.pl /mnt/installer-root/root/");
+# system("cp ./perl/garchinstall3.pl /mnt/installer-root/root/");
+# system("arch-chroot /mnt/installer-root pacman -Syu");
+# system("arch-chroot /mnt/installer-root pacman -Sy glib2 pango gdk-pixbuf2 gtk2 gtk3 perl gtk2-perl perl-gtk3 weston --noconfirm");
+# system("arch-chroot /mnt/installer-root perl /root/garchinstall2.pl");
+
+print("Starting gui_backend_server...");
+system("chmod +x ../gui_backend_server")
+system("../gui_backend_server &");
