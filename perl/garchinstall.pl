90e9872f9f5663953234104e450d695b62f1b881
Edward Halferty <edhalferty@gmail.com>
2021-10-21 17:46:34 -0700

    stuff

--- perl/garchinstall.pl
+++ perl/garchinstall.pl
@@ -1,21 +1,45 @@
 print("Downloading packages for garchinstaller... (This may take awhile)");
 # TODO: Check if we have network connectivity. If not, run the framebuffer-based network manager
 # TODO: Throw up a splash screen to tell the user that we're downloading dependencies...
-$tmpRt = "/mnt/installer-root";
-$chrt = "arch-chroot ${tmpRt}";
-$pcmn = "pacman -Sy";
-$dependencies = "xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware xf86-video-fbdev
-    xf86-input-evdev xf86-input-synaptics weston lightdm perl-gtk3";
-
-system("mkdir", $tmpRt, ">/dev/null 2>&1");
-system("mount -t tmpfs -o size=4g tmpfs", $tmpRt, ">/dev/null 2>&1");
-system("pacstrap", $tmpRt, "base linux >/dev/null 2>&1");
-system($chrt, "pacman -Syu >/dev/null 2>&1");
-system($chrt, $pcmn, $dependencies, "--noconfirm >/dev/null 2>&1");
+system("mkdir /mnt/installer-root >/dev/null 2>&1");
+system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root >/dev/null 2>&1");
+system("pacstrap /mnt/installer-root base linux >/dev/null 2>&1");
+system("arch-chroot /mnt/installer-root pacman -Syu >/dev/null 2>&1");
+system("arch-chroot /mnt/installer-root pacman -Sy xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server
+    xf86-video-vmware xf86-video-fbdev xf86-input-evdev xf86-input-synaptics weston lightdm perl-gtk3
+    --noconfirm >/dev/null 2>&1");
+system("arch-chroot /mnt/installer-root pacman -Sy  --noconfirm >/dev/null 2>&1");
 system("unset DBUS_SESSION_BUS_ADDRESS");
+system("arch-chroot /mnt/installer-root systemctl enable lightdm >/dev/null 2>&1");
 # TODO: Remove splash screen before starting XFCE
-system($chrt, "startxfce4 >/dev/null 2>&1");
-system("/usr/bin/perl", "garchinstall2.pl")
+system("arch-chroot /mnt/installer-root startxfce4 >/dev/null 2>&1");
+system("/usr/bin/perl", "perl/garchinstall2.pl");
+
+# mount -t tmpfs -o size=512m tmpfs /mnt/root1
+# pacstrap /mnt/root1 base linux
+# arch-chroot /mnt/root1
+# pacman -Syu
+# pacman -Sy gcc vim
+
+
+# print("Downloading packages for garchinstaller... (This may take awhile)");
+# # TODO: Check if we have network connectivity. If not, run the framebuffer-based network manager
+# # TODO: Throw up a splash screen to tell the user that we're downloading dependencies...
+# $tmpRt = "/mnt/installer-root";
+# $chrt = "arch-chroot ${tmpRt}";
+# $pcmn = "pacman -Sy";
+# $dependencies = "xorg xfce4 xorg-xinit xorg-xeyes xf86-video-fbdev xorg-server xf86-video-vmware xf86-video-fbdev
+#     xf86-input-evdev xf86-input-synaptics weston lightdm perl-gtk3";
+
+# system("mkdir", $tmpRt, ">/dev/null 2>&1");
+# system("mount -t tmpfs -o size=4g tmpfs", $tmpRt, ">/dev/null 2>&1");
+# system("pacstrap", $tmpRt, "base linux >/dev/null 2>&1");
+# system($chrt, "pacman -Syu >/dev/null 2>&1");
+# system($chrt, $pcmn, $dependencies, "--noconfirm >/dev/null 2>&1");
+# system("unset DBUS_SESSION_BUS_ADDRESS");
+# # TODO: Remove splash screen before starting XFCE
+# system($chrt, "startxfce4 >/dev/null 2>&1");
+# system("/usr/bin/perl", "perl/garchinstall2.pl")
 
 # use Gtk3 -init;
 # my $hello = Gtk3::MessageDialog->new (undef, 'modal', 'info', 'ok', "Hello world!");
