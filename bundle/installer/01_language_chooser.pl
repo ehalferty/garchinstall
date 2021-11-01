5912ef9fe7cab2d441287e6039c1e779c307c93e
Edward Halferty <edhalferty@gmail.com>
2021-11-01 04:30:35 -0700

    stuff

--- bundle/installer/01_language_chooser.pl
+++ bundle/installer/01_language_chooser.pl
@@ -85,7 +85,7 @@ sub draw_bmp {
 draw_rect(0, 400, 400, 10);
 draw_text(100, 100, "Hello, world!");
 my $arch_logo_ref = load_bmp("bundle/images/archlogo65.png");
-print "Return from load_bmp\n";
+print "Return val from load_bmp:\n";
 print map { sprintf '%02X ', ord } split //, $arch_logo_ref;
 print "\n";
 draw_bmp(150, 150, 65, 65, $arch_logo_ref);
