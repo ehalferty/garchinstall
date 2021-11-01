eb44d21f89a271df8eb972f5118f2670234f94eb
Edward Halferty <edhalferty@gmail.com>
2021-11-01 04:55:14 -0700

    stuff

--- gui_server/gui_server.c
+++ gui_server/gui_server.c
@@ -388,7 +388,7 @@ void HandleMessage() {
                     ((uint8_t)(tm[idx + 12]) << 32) + ((uint8_t)(tm[idx + 13]) << 40) +
                     ((uint8_t)(tm[idx + 14]) << 48) + ((uint8_t)(tm[idx + 15]) << 56);
                 printf("MSG_DRAW_BITMAP x=%d y=%d w=%lu h=%lu bmp=%08llx\n", x, y, w, h, bmp); fflush(stdout);
-                DrawBitmap(x, y, w, h, bmp);
+                // DrawBitmap(x, y, w, h, bmp);
                 printf("OKAY OKAY OKAY\n");
                 idx += 12;
                 break; }
