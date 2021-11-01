3c6a8c4a8869699b84f0e42ebfc543244766e051
Edward Halferty <edhalferty@gmail.com>
2021-11-01 04:11:32 -0700

    stuff

--- bundle/installer/01_language_chooser.pl
+++ bundle/installer/01_language_chooser.pl
@@ -15,7 +15,7 @@ sub send_msg {
     print "1\n";
     my $client = IO::Socket::UNIX->new(Type => SOCK_STREAM(), Peer => $SOCK_PATH);
     my $len = length(@_[1]) + 5;
-    printf("Sending a message with len=%d\n", $len);
+    printf("Sending a message with id=%d len=%d\n", @_[0], $len);
     my $msg = sprintf("%c%c%c%c\x01\x00%c%c%s\x00", $len & 0xFF, ($len >> 8) & 0xFF, ($len >> 16) & 0xFF,
         ($len >> 24) & 0xFF, @_[0] & 0xFF, (@_[0] >> 8) & 0xFF, @_[1]);
     print "2\n";
