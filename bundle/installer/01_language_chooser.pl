3b4cb098f61e4e8acadecaa1c3cc280f79921352
Edward Halferty <edhalferty@gmail.com>
2021-10-31 21:17:01 -0700

    stuff

--- bundle/installer/01_language_chooser.pl
+++ bundle/installer/01_language_chooser.pl
@@ -3,6 +3,6 @@ use IO::Socket::UNIX;
 my $SOCK_PATH = "/tmp/gui_server_socket";
 my $client = IO::Socket::UNIX->new(Type => SOCK_STREAM(), Peer => $SOCK_PATH);
 # print {$client} '\x03\x00\x00\x00\x01\x00\x01';
-print {$client} "\x0B\x00\x00\x00" + "\x01\x00" + "\x05\x00" + "\x05\x00" + "ABCD\x00";
+print {$client} "\x0B\x00\x00\x00\x01\x00\x05\x00\x05\x00ABCD\x00";
 print "Got reponse: ", scalar <$client>;
 close $client;
