0920a713c214b416419c3ad46ac47cf5f16808e7
Edward Halferty <edhalferty@gmail.com>
2021-11-01 04:45:32 -0700

    stuff

--- bundle/installer/01_language_chooser.pl
+++ bundle/installer/01_language_chooser.pl
@@ -74,7 +74,7 @@ sub draw_bmp {
     #     $addr & 0xFF, ($addr >> 8) & 0xFF, ($addr >> 16) & 0xFF, ($addr >> 24) & 0xFF,
     #     ($addr >> 32) & 0xFF, ($addr >> 40) & 0xFF, ($addr >> 48) & 0xFF, ($addr >> 56) & 0xFF);
     printf("About to send MSG_DRAW_BITMAP\n");
-    my $msg = pack('S<4Q<', $x, $y, $w, $h, $addr);
+    my $msg = pack('S<4Q<', $x, $y, $w, $h) . $addr;
     print map { sprintf '%02X ', ord } split //, $msg;
     print "\n";
     my $r1 = send_msg(MSG_DRAW_BITMAP, $msg);
