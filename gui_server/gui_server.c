625f614efbefe2cb79da403e5f639576f692806f
Edward Halferty <edhalferty@gmail.com>
2021-11-02 05:44:32 -0700

    stuff

--- gui_server/gui_server.c
+++ gui_server/gui_server.c
@@ -194,10 +194,10 @@ void DrawRoundedRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t r)
     DrawRect(x, y + r, w, h - r * 2);
     DrawRect(x + r, y, w - r * 2, r);
     DrawRect(x + r, y + h - r, w - r * 2, r);
-    DrawCircle(x, y, r, 0);
-    DrawCircle(x + w, y, r, 0);
-    DrawCircle(x, y + h, r, 0);
-    DrawCircle(x + w, y + h, r, 0);
+    DrawCircle(x, y, r + 3, 0);
+    DrawCircle(x + w, y, r + 3, 0);
+    DrawCircle(x, y + h, r + 3, 0);
+    DrawCircle(x + w, y + h, r + 3, 0);
 }
 void DrawRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
     int i, j, xx, yy;
