19554c20b2d3a807ad4dce5252d466ac8df48bc8
Edward Halferty <edhalferty@gmail.com>
2021-11-02 22:01:44 -0700

    stuff

--- gui_server/gui_server.c
+++ gui_server/gui_server.c
@@ -167,6 +167,7 @@ void DrawCirclePixels(uint32_t xc, uint32_t yc, uint32_t x, uint32_t y, uint32_t
             DrawPixel(xc - x, yc - y, r, g, b);
             DrawPixel(xc - y, yc - x, r, g, b);
             for (i = xc - x; i < xc; i++) { DrawPixel(i, yc - y, r, g, b); }
+            for (i = xc - y; i < xc; i++) { DrawPixel(i, yc - x, r, g, b); }
             break;
         }
         case 1: { DrawPixel(xc + x, yc - y, r, g, b); DrawPixel(xc + y, yc - x, r, g, b); break; }
