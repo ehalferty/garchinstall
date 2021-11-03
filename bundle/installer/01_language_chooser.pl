6f0da9b4b902fe3aefdaab15c27269c8f58d00a4
Edward Halferty <edhalferty@gmail.com>
2021-11-03 02:24:51 -0700

    stuff

--- bundle/installer/01_language_chooser.pl
+++ bundle/installer/01_language_chooser.pl
@@ -83,7 +83,7 @@ sub get_events {
 }
 
 sub get_resolution {
-    my ($x, $y, $bpp) = unpack('S<S<C', split(send_msg(MSG_GET_RESOLUTION, ""), 8));
+    my ($x, $y, $bpp) = unpack('S<S<C', split(send_msg(MSG_GET_RESOLUTION, ""), 10));
 
 
     # my @res = split(send_msg(MSG_GET_RESOLUTION, ""));
