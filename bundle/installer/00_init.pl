bbf0a9c919f21f00c2d7618c0bde68f8b336c31f
Edward Halferty <edhalferty@gmail.com>
2021-11-07 21:18:20 -0800

    checkin

--- bundle/installer/00_init.pl
+++ bundle/installer/00_init.pl
@@ -1,6 +1,21 @@
-# Prints welcome, starts gui server
-
+# ssh root@192.168.56.102
 print("Welcome to garchinstaller!\n");
+
+system("stty -echo");
+
+print("Increasing temporary filesystem size to 4GB (I hope you have the RAM buddy!)...\n");
+system("echo \"12e45\n12e45\" | passwd root");
+system("mount -o remount,size=4G /run/archiso/cowspace");
+
+print("Downloading & installing a bunch of graphics packages...\n");
+system("pacman -Syu --noconfirm");
+system("pacman -Syy");
+system("pacman -Sy libffi xf86-video-fbdev xf86-video-vmware xf86-input-libinput xorg-server xorg-xinit xorg-xclock --noconfirm");
+# system("pacman -Sy glib2 pango gdk-pixbuf2 gtk2 gtk3 perl gtk2-perl perl-gtk3 weston --noconfirm");
+
+
+
+
 # system("mkdir /mnt/installer-root");
 # system("mount -t tmpfs -o size=4g tmpfs /mnt/installer-root");
 # system("pacstrap /mnt/installer-root base linux");
@@ -10,11 +25,14 @@ print("Welcome to garchinstaller!\n");
 # system("arch-chroot /mnt/installer-root pacman -Sy glib2 pango gdk-pixbuf2 gtk2 gtk3 perl gtk2-perl perl-gtk3 weston --noconfirm");
 # system("arch-chroot /mnt/installer-root perl /root/garchinstall2.pl");
 
-system("stty -echo");
-system("killall gui_server");
-print("Starting gui_server...\n");
-system("chmod +x ./bundle/gui_server");
-system("./bundle/gui_server &> /tmp/serverlog.txt &");
+# system("killall gui_server");
+# print("Starting gui_server...\n");
+# system("chmod +x ./bundle/gui_server");
+# system("./bundle/gui_server &> /tmp/serverlog.txt &");
+
+
+
+
 # system("perl -Ibundle/installer ./bundle/installer/01_language_chooser.pl");
 # system("perl ./bundle/")
 # TODO: gui_server should listen on a socket
